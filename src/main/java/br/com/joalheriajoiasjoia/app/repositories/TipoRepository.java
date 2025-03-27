46d59fb686edea1684b1dfe8049bac2630312c7f
EveAchnitz <eve.jo.achnitz@gmail.com>
2025-03-27 12:44:08 -0300

    Alterações no usuário e no tipo usuário

--- src/main/java/br/com/joalheriajoiasjoia/app/repositories/TipoRepository.java
+++ src/main/java/br/com/joalheriajoiasjoia/app/repositories/TipoRepository.java
@@ -1,6 +1,6 @@
 package br.com.joalheriajoiasjoia.app.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository; 
 
 import br.com.joalheriajoiasjoia.app.entities.Tipo;
 
