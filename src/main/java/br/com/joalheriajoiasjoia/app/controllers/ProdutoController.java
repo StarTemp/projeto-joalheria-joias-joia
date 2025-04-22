f5b7944aaa9f89f2bae24f4423396ef1e152dfa7
EveAchnitz <eve.jo.achnitz@gmail.com>
2025-04-22 09:40:30 -0300

    Correção da página do produto

--- src/main/java/br/com/joalheriajoiasjoia/app/controllers/ProdutoController.java
+++ src/main/java/br/com/joalheriajoiasjoia/app/controllers/ProdutoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,7 +34,7 @@ public class ProdutoController {
 	}
 	
 	@GetMapping("/{id}")
-	public Produto getProdutoById(Long id) {
+	public Produto getProdutoById(@PathVariable Long id) {
 		return produtoService.getProdutoById(id);
 	}
 	
